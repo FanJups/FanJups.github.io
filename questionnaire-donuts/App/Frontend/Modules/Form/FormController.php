877fd871ba277f0820b4b6acf945ae4037931f1d
Fanon JUPKWO <jupsfan@gmail.com>
2018-12-06 15:16:30 +0100

    Update FormController.php

--- questionnaire-donuts/App/Frontend/Modules/Form/FormController.php
+++ questionnaire-donuts/App/Frontend/Modules/Form/FormController.php
@@ -9,7 +9,7 @@ use \DONUTSFram\FormHandler;
 
 class FormController extends BackController   
 {
-  public function executeIndex(HTTPRequest $request)
+  public function executeIndex(HTTPRequest $request)  
   {
      
     
@@ -29,8 +29,8 @@ class FormController extends BackController
     // On ajoute une définition pour le titre.
     $this->page->addVar('title', 'Mon poids et Moi : Email');
 
-    //Pour uniquement utiliser donuts.js
-    //$this->page->addVar('donuts', 'index');
+//Pour uniquement utiliser emailincorrect.js
+    $this->page->addVar('donutsEmail', 'email');
   
    
   }
