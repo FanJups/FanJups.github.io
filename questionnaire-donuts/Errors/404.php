38191ac8ba8bcd091acb0183bc35e6c818ab9f1b
Fanon JUPKWO <jupsfan@gmail.com>
2018-12-06 15:11:16 +0100

    Add files via upload

---
+++ questionnaire-donuts/Errors/404.php
@@ -0,0 +1,26 @@
+
+
+<br>
+
+<div  id="errorpagenotfound" class="row">   
+
+			<div class="col-xs-12"> 
+
+				<p>
+					&nbsp; &nbsp;  Cette page que vous tentez d'accéder est introuvable. Vous pouvez nous contacter (email : <span>contact@donuts-asso.org</span>) si vous pensez qu'il s'agit d'un bug.
+
+				</p>
+
+				<br> 
+			 
+
+				<strong style=" font-size: 2em ; "> <i   class="fa fa-frown-o sad" aria-hidden="true"></i></strong>
+
+			</div> 
+			
+
+
+</div>
+
+<br>
+
