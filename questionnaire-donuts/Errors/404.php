39a029267b007145bdf5124998c56cdde85b5757
Fanon JUPKWO <jupsfan@gmail.com>
2018-12-15 11:59:21 +0100

    Adding a home link

--- questionnaire-donuts/Errors/404.php
+++ questionnaire-donuts/Errors/404.php
@@ -2,19 +2,23 @@
 
 <br>
 
-<div  id="errorpagenotfound" class="row">   
+<div  id="errorpagenotfound" class="row">    
 
 			<div class="col-xs-12"> 
 
 				<p>
-					&nbsp; &nbsp;  Cette page que vous tentez d'accéder est introuvable. Vous pouvez nous contacter (email : <span>contact@donuts-asso.org</span>) si vous pensez qu'il s'agit d'un bug.
+					&nbsp; &nbsp;  Cette page que vous tentez d'accéder est introuvable. Vous pouvez nous contacter (email : <span>contact@donuts-asso.org</span>) si vous pensez qu'il s'agit d'un bug. En cliquant sur le bouton Accueil, vous avez accès au questionnaire.
 
 				</p>
 
-				<br> 
+				<br>   
 			 
 
 				<strong style=" font-size: 2em ; "> <i   class="fa fa-frown-o sad" aria-hidden="true"></i></strong>
+
+				<div style="margin: 10px 0 10px 0;"></div> <!-- Just want to create space-->
+
+				<a href="/" class="btn btn-warning">Accueil</a>
 
 			</div> 
 			
